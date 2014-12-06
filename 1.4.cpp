4e607e3af8f98bd6b41ba41eb46a078c7ee179f1
chester39 <chester39@163.com>
2014-12-06 23:50:20 +0800

    Create 1.4.cpp

---
+++ 1.4.cpp
@@ -0,0 +1,26 @@
+/**************************************************
+ *              Essential C++                     *
+ *                Stanley Lippman                 *
+ *                Chen Chen @ November 26th, 2014 *
+ **************************************************/
+ 
+ 
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	vector<string> vecName(2);
+	cout << "Please enter your first name: ";
+	cin >> vecName[0];
+	cout << "Please enter your last name: ";
+	cin >> vecName[1];
+	for (vector<string>::iterator itr = vecName.begin(), vecEnd = vecName.end(); itr != vecEnd; itr++)
+	{
+		cout << *itr << " ";
+	}
+	return 0;
+}
