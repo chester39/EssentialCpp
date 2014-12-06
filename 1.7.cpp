c2cec53100435dd73830298d42df48612fdd853d
chester39 <chester39@163.com>
2014-12-07 00:02:44 +0800

    Create 1.7.cpp

---
+++ 1.7.cpp
@@ -0,0 +1,41 @@
+/**************************************************
+ *              Essential C++                     *
+ *                Stanley Lippman                 *
+ *                Chen Chen @ November 27th, 2014 *
+ **************************************************/
+ 
+ 
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	ifstream readFile("TestFile_1.7.txt");
+	if (! readFile.is_open())
+	{
+		cerr << "Sorry, the file fails to read!" << endl;
+	}
+	string word;
+	vector<string> vecWord;
+	while (read_file >> word)
+	{
+		vecWord.push_back(word);
+	}
+	cout << "Original text: ";
+	for (vector<string>::iterator itr = vecWord.begin(), vecEnd = vecWord.end(); itr != vecEnd; itr++)
+	{
+		cout << *itr << " ";
+	}
+	sort(vecWord.begin(),vecWord.end());
+	cout << endl << "Sorted text: ";
+	for (vector<string>::iterator itr = vecWord.begin(), vecEnd = vecWord.end(); itr != vecEnd; itr++)
+	{
+		cout << *itr << " ";
+	}
+	return 0;
+}
