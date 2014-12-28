dcaa8b6193015ba493786b1c5311b84270c74614
Chester Chen <chester39@163.com>
2014-12-28 11:29:39 +0800

    Create 1.8.cpp

---
+++ Chapter 1. Basic C++ Programming/1.8.cpp
@@ -0,0 +1,24 @@
+/**************************************************
+ *              Essential C++                     *
+ *                Stanley Lippman                 *
+ *                Chen Chen @ November 26th, 2014 *
+ **************************************************/
+ 
+ 
+const char *MessageToUser(int numTries)
+{
+	const int num = 5;
+	static const char *userMessage[num] = 
+	{
+		"Go on, make a guess.",
+		"Oops! Nice guess but not quite it.",
+		"Oh. Sorry. Wrong again.",
+		"Ah, this is harder than it looks, no?",
+		"It must be getting pretty frustrating by now!"
+	};
+	if (numTries < 0)
+		numTries = 0;
+	else if (numTries >= num)
+		numTries = num - 1;
+	return userMessage[numTries];
+}
