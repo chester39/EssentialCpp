a0adb6e073f2111ecdab9e4644979a36ddbbc34d
Chester Chen <chester39@163.com>
2014-12-28 11:28:19 +0800

    Create 1.1.cpp

---
+++ Chapter 1. Basic C++ Programming/1.1.cpp
@@ -0,0 +1,20 @@
+/**************************************************
+ *              Essential C++                     *
+ *                Stanley Lippman                 *
+ *                Chen Chen @ November 26th, 2014 *
+ **************************************************/
+ 
+ 
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	string userName;
+	cout << "Please enter your first name: ";
+	cin >> userName;
+	cout << endl << "Hello, " << userName << "... and goodbye!" << endl;
+	return 0;
+}
