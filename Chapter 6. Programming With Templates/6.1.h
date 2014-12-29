efbb49e93b2ad66e9ed486f8d0c50dd6935be23c
chester39 <chester39@163.com>
2014-12-29 23:49:55 +0800

    Start Chapter 6

---
+++ Chapter 6. Programming With Templates/6.1.h
@@ -0,0 +1,40 @@
+/**************************************************
+ *              Essential C++                     *
+ *                Stanley Lippman                 *
+ *                Chen Chen @ December 29th, 2014 *
+ **************************************************/
+ 
+ 
+#ifndef EXAMPLE_H_
+#define EXAMPLE_H_
+
+template <typename Type>
+class Example
+{
+public:
+	Example(const Type &min, const Type &max);
+	Example(const Type *array, int size);
+	Type opeartor[](int index);
+	bool opeartor == (const Example &) const;
+	bool Insert(const Type *, int);
+	bool Insert(Type &);
+	Type Min() const
+	{
+		return _min;
+	}
+	Type Max() const
+	{
+		return _max;
+	}
+	void Min(const Type &);
+	void Max(const Type &);
+	int Count(const Type &value) const;
+
+private:
+	int _size;
+	Type *parray;
+	Type _min;
+	Type _max;
+};
+
+#endif
